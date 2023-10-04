ac72aab37f1f3356150a813fa62a963288bf1421
SM Feroj <sm.feroj@gmail.com>
2023-10-04 23:15:22 +0600

    first commit

---
+++ product.php
@@ -0,0 +1,42 @@
+
+<?
+
+class Product {
+    private $id;
+    private $name;
+    private $price;
+
+    public function __construct($id, $name, $price) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    
+    public function getFormattedPrice() {
+        return '$' . number_format($this->price, 2);
+    }
+
+
+    public function showDetails() {
+        echo "Product Details:\n";
+        echo "- ID: {$this->id}\n";
+        echo "- Name: {$this->name}\n";
+        echo "- Price: {$this->getFormattedPrice()}\n";
+    }
+}
+
+
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+
+
+/*
+
+Product Details:
+- ID: 1
+- Name: T-shirt
+- Price: $19.99
+
+
+*/
